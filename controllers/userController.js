1620351cc209f3dae59e0299c8c44099eeae3547
Jatin <jatin@daxko.com>
2023-07-07 21:35:30 +0530

    sign in functionality done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,52 +1,75 @@
-const User = require("../models/user")
+const User = require("../models/user");
 
-exports.signup = async(req, res)=>{
-    try {
-        console.log(req.body)
-        const{username, email, password} = req.body
+exports.signup = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, email, password } = req.body;
 
-        //find user if exist or not
-        let user = await User.findOne({email})
-        
-        //return if user not exist
-        if(user){
-            return res.status(400).json({
-                success:false,
-                message: "User already exists"
-            })
-        }
+    //find user if exist or not
+    let user = await User.findOne({ email });
 
-        //user created
-        user = await User.create({username, email, password})
-
-        await user.save()
-        
-        //generate token
-        const token = user.generateToken()
-
-        res.status(200).json({
-            user: user,
-            token:token
-        })
-        
-    } catch (err) {
-        
-        res.status(500).json({
-            success: false,
-            message: err.message,
-          });
+    //return if user not exist
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
     }
-}
 
-exports.signIn = async(req, res)=>{
-    try {
-        
-    } catch (err) {
+    //user created
+    user = await User.create({ username, email, password });
 
-        res.status(500).json({
-            success:false,
-            message:err.message
-        })
-        
+    await user.save();
+
+    //generate token
+    const token = user.generateToken();
+
+    res.status(201).json({
+      user: user,
+      token: token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+
+//sign in method
+exports.signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email }).select("+password");
+
+    //check user exist or not
+    if (!user) {
+      return res.status(500).json({
+        success: false,
+        message: "User doesn't exist",
+      });
     }
-}
+
+    //now check password
+    const isMatch = user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Credentionls",
+      });
+    }
+
+    const token = user.generateToken();
+
+    res.status(201).json({
+      user: user,
+      token: token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const { signup } = require("../controllers/userController")
+const { signup, signIn } = require("../controllers/userController")
 const userRouter = express.Router()
 
 //signup route
 userRouter.route("/signup").post(signup)
 
-userRouter.route("/signin").post()
+userRouter.route("/signin").post(signIn)
 
 
 module.exports = userRouter
